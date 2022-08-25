875ee9691a6890fc38b59e4b14f2877e6eacb83e
maxim.ulshin <maxim.ulshin@cyper.ca>
2022-08-25 19:51:08 +0300

    make production ready classnames generated by css-in-js

--- container/src/App.js
+++ container/src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
 import { MarketingApp } from "./components/MarketingApp";
+import { Header } from "./components/Header";
+import { BrowserRouter } from "react-router-dom";
+import { StylesProvider } from "@material-ui/styles";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co"
+});
 export const App = () => {
   return (
-    <div>
-      <h1>Hi there!!!</h1>
-      <br /> <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
   );
 };
--- marketing/src/App.js
+++ marketing/src/App.js
@@ -1,11 +1,16 @@
-import { StylesProvider } from "@material-ui/styles";
+import { createGenerateClassName, StylesProvider } from "@material-ui/styles";
 import React from "react";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
 import Pricing from "./components/Pricing";
 import Landing from "./components/Landing";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "ma"
+});
+
 export const App = () => {
   return (
-    <StylesProvider>
+    <StylesProvider generateClassName={generateClassName}>
       <BrowserRouter>
         <Switch>
           <Route exact path="/pricing" component={Pricing} />
@@ -16,4 +21,3 @@ export const App = () => {
     </StylesProvider>
   );
 };
-
