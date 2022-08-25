135c070d54a8e5b486f02e3344a07bdca46baba8
maxim.ulshin <maxim.ulshin@cyper.ca>
2022-08-25 20:11:21 +0300

    resolved errors in container and marketing

--- marketing/src/App.js
+++ marketing/src/App.js
@@ -1,8 +1,8 @@
-import StylesProvider, { createGenerateClassName } from "@material-ui/styles";
+import { StylesProvider, createGenerateClassName } from "@material-ui/styles";
 import React from "react";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
-import Pricing from "./components/Pricing";
-import Landing from "./components/Landing";
+import { Pricing } from "./components/Pricing";
+import { Landing } from "./components/Landing";
 
 const generateClassName = createGenerateClassName({
   productionPrefix: "ma"
